57a00a0f00264ec4ddbfe1f579f3c9dca34bdc0d
narcissus123 <haeri.n@yahoo.com>
2024-11-27 11:15:05 -0500

    update indentation by running lint.

--- app.tsx
+++ app.tsx
@@ -13,7 +13,7 @@ const app: Express = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"]
   })
 );
 
