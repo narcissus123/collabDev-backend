7e28542237b3eaff42a73272f7dc48adce5a41bf
narcissus123 <haeri.n@yahoo.com>
2024-11-27 11:22:57 -0500

    Add resume upload, retrieval, and deletion routes to user router.

--- routes/userRoute.tsx
+++ routes/userRoute.tsx
@@ -3,10 +3,11 @@ import * as userController from "../controllers/userController";
 import * as authController from "../controllers/authController";
 import { userDataConvertor } from "../middleware/userDataConvertor.middleware";
 import { upload } from "../middleware/fileUpload.middleware";
+import { uploadToS3 } from "../middleware/s3Upload.middleware";
 
 const uploadUserImages = upload.fields([
   { name: "avatar", maxCount: 1 },
-  { name: "badges", maxCount: 12 },
+  { name: "badges", maxCount: 12 }
 ]);
 
 const router: Router = Router();
@@ -21,4 +22,12 @@ router
     userController.UpdateUser
   );
 
-export default router;
+router
+  .route("/resume/:userId")
+  .get(userController.getResumeUrl) // Get presigned URL for viewing
+  .post(
+    authController.protect,
+    uploadToS3.single("file"),
+    userController.uploadResume
+  )
+  .delete(authController.protect, userController.deleteResume);
