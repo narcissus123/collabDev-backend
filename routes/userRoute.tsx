8a007199f20855b611a4cc3e104ebd8684bc48bf
narcissus123 <haeri.n@yahoo.com>
2024-12-03 11:15:09 -0500

    Removed unused middlewares.

--- routes/userRoute.tsx
+++ routes/userRoute.tsx
@@ -1,14 +1,6 @@
 import { Router } from "express";
 import * as userController from "../controllers/userController";
 import * as authController from "../controllers/authController";
-import { userDataConvertor } from "../middleware/userDataConvertor.middleware";
-import { upload } from "../middleware/fileUpload.middleware";
-import { uploadToS3 } from "../middleware/s3Upload.middleware";
-
-const uploadUserImages = upload.fields([
-  { name: "avatar", maxCount: 1 },
-  { name: "badges", maxCount: 12 }
-]);
 
 const router: Router = Router();
 router.route("/").get(userController.getAllUsers);
@@ -17,17 +9,7 @@ router
   .get(userController.getUserById)
   .put(
     authController.protect,
-    uploadUserImages,
-    userDataConvertor,
     userController.UpdateUser
   );
 
-router
-  .route("/resume/:userId")
-  .get(userController.getResumeUrl) // Get presigned URL for viewing
-  .post(
-    authController.protect,
-    uploadToS3.single("file"),
-    userController.uploadResume
-  )
-  .delete(authController.protect, userController.deleteResume);
+export default router;
