ec65b86656c539d913322609501b98eac532dab8
Narges Haeri <haeri.n@yahoo.com>
2024-12-03 23:58:15 -0500

    Update configuration. (#5)

--- app.tsx
+++ app.tsx
@@ -15,8 +15,7 @@ const app: Express = express();
 
 app.use(
   cors({
-    origin: config.corsOrigins,
-    credentials: true
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"]
   })
 );
 
