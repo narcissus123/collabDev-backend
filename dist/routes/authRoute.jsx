7bcb0e906d69cec481903cda86ce16a1b3a0f78e
narcissus123 <haeri.n@yahoo.com>
2024-12-04 11:51:49 -0500

    build the app

--- dist/routes/authRoute.jsx
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import * as authController from "../controllers/authController";
-const router = express.Router();
-router.route("/signup").post(authController.signup);
-router.route("/login").post(authController.login);
-router.route("/change-password").post(authController.forgetPassword);
-export default router;
--- dist/routes/contributionRoute.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import * as contributionController from "../controllers/contributionController";
-// import express from "express";
-// const router = express.Router();
-// router.get(
-//   "/projects/:projectId/contributors",
-//   contributionController.findContributorsForProject
-// );
-// module.exports = router;
