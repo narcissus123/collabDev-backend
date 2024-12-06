d8d2ead161f744040590e4cd24f5e2994a2b551f
Narges Haeri <haeri.n@yahoo.com>
2024-12-06 14:18:54 -0500

    Trigger new deployment (#23)

--- dist/server.js
+++ dist/server.js
@@ -6,7 +6,7 @@ import { ChatMessage } from "./models/chatMessageModel.js";
 import { createServer } from "node:http";
 import { updateMongoIPWhitelist } from './utils/mongoIPManager';
 console.log('Current NODE_ENV:', process.env.NODE_ENV);
-console.log('Loading config from:', process.env.NODE_ENV === 'production' ? '.env.production' : '.env');
+console.log('Loading config from here:', process.env.NODE_ENV === 'production' ? '.env.production' : '.env');
 dotenv.config({
     path: process.env.NODE_ENV === 'production'
         ? '.env.production'
